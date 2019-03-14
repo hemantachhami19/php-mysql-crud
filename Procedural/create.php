6d9348a42e4e8bc8dc3db29d65efa3bea9133dbf
Hemant Achhami <hemantachhami19@gmail.com>
2019-03-14 22:53:18 +0545

    gitignore updated

--- Procedural/create.php
+++ Procedural/create.php
@@ -3,50 +3,55 @@
 require_once "config.php";
 
 // Define variables and initialize with empty values
-$description = "" ;
+$description = "";
 $description_err = "";
 
+$status = "";
+$status_err = "";
+
 // Processing form data when form is submitted
-if($_SERVER["REQUEST_METHOD"] == "POST"){
-    // Validate name
-    $input_name = trim($_POST["description"]);
-    if(empty($input_name)){
-        $name_err = "Cannot add empty task";
-    } elseif(!filter_var($input_name, FILTER_VALIDATE_REGEXP, array("options"=>array("regexp"=>"/^[a-zA-Z\s]+$/")))){
-        $name_err = "Please enter a valid description.";
-    } else{
-        $name = $input_name;
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+    // Validate task description
+    $input_description = trim($_POST["description"]);
+    if (empty($input_description)) {
+        $description_err = "Cannot add empty task";
+    } elseif (!filter_var($input_description, FILTER_VALIDATE_REGEXP, array("options" => array("regexp" => "/^[a-zA-Z\s]+$/")))) {
+        $description_err = "Please enter a valid description.";
+    } else {
+        $description = $input_description;
     }
 
-    // Validate salary
-    $input_status = trim($_POST["salary"]);
-    if(empty($input_status)){
-        $input_status = "Please enter the valid status.";
-    } elseif(!$bool = filter_var($input_status, FILTER_VALIDATE_BOOLEAN)){
-        $status_err = "Please enter a positive integer value.";
-    } else{
-        $status = $input_status;
+    //Validate status
+    $input_status = trim($_POST["status"]);
+    if(!isset($input_status)){
+        $status_err = "Enter the validate status";
+    }else{
+        $status = filter_var($input_status, FILTER_VALIDATE_BOOLEAN);
+        if(is_null($status)){
+            $status_err = "Enter the valid status";
+        }
     }
 
     // Check input errors before inserting in database
-    if(empty($name_err) && empty($status_err) && empty($salary_err)){
+    if (empty($description_err) && empty($status_err)) {
         // Prepare an insert statement
         $sql = "INSERT INTO tasks (description, status) VALUES (?, ?)";
 
-        if($stmt = mysqli_prepare($link, $sql)){
+        if ($stmt = mysqli_prepare($link, $sql)) {
             // Bind variables to the prepared statement as parameters$param_address,
-                mysqli_stmt_bind_param($stmt, "ss", $param_name,  $param_salary);
+            mysqli_stmt_bind_param($stmt, "si", $param_name, $param_status);
 
             // Set parameters
-            $param_name = $name;
-            $param_status = $status;
+            $param_name = $description;
+            $param_status = intval($status);
 
             // Attempt to execute the prepared statement
-            if(mysqli_stmt_execute($stmt)){
+            if (mysqli_stmt_execute($stmt)) {
                 // Records created successfully. Redirect to landing page
                 header("location: index.php");
                 exit();
-            } else{
+            } else {
                 echo "Something went wrong. Please try again later.";
             }
         }
